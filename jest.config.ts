c79cc507e0fc265af63c90c09a5410acdf3fcfa8
Leon Linhart <themrmilchmann@gmail.com>
2023-05-23 09:54:36 +0200

    build: include "js" module file extension in jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import {JestConfigWithTsJest} from "ts-jest";
 
 const jestConfig: JestConfigWithTsJest = {
   clearMocks: true,
-  moduleFileExtensions: ["ts"],
+  moduleFileExtensions: ["js", "ts"],
   testEnvironment: "node",
   transform: {
     '^.+\\.ts$': [
